700f9331b1cd013049b400a72b38943e58804694
corgi <creep3rcor7@gmail.com>
2023-02-11 09:03:24 -0500

    Session Stored!

--- src/utils/createApp.ts
+++ src/utils/createApp.ts
@@ -3,6 +3,7 @@ import cors from 'cors'
 import session from 'express-session'
 import routes from '../routes'
 import passport from 'passport'
+import store from 'connect-mongo'
 
 export function createApp(): Express {
     require('../strategies/discord');
@@ -13,12 +14,14 @@ export function createApp(): Express {
 
     // Enabling CORS
     app.use(cors({ origin: ["http://localhost:3000"], credentials: true, }));
+    // Sessions
     app.use(session(
         {
             secret: "DiscordDashboardByCorgi",
             resave: false,
             saveUninitialized: false,
             cookie: { maxAge: 60000 * 60 * 24 * 7, },
+            store: store.create({ mongoUrl: `${process.env.MONGO_URI}` })
         })
     )
 
